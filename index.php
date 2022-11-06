b5103d6a7950299c934d0270a25fe2d1c94789f6
sempere <samueljose.garcia@iesdonana.org>
2022-11-06 19:33:23 +0100

    funcionando el pintado de la biblioteca

--- index.php
+++ index.php
@@ -10,7 +10,23 @@
     <?php
         $pdo = new PDO("pgsql:host=localhost;dbname=biblioteca", "biblioteca", "biblioteca");
 
-        var_dump($pdo);
+        $sent = $pdo->query("SELECT * FROM biblioteca");
+
+        $filas = $sent->fetchAll();
     ?>
+    <h1 style="text-align: center;">BIBLIOTECAS</h1>
+    <table border="1" style="margin: auto;">
+        <th>Código</th>
+        <th>denominación</th>
+        <th>Posición</th>
+        <?php foreach ($filas as $fila): ?>
+        <tr>
+            <td><?= $fila['codigo'] ?></td>
+            <td><?= $fila['denominacion'] ?></td>
+            <td><?= $fila['posicion'] ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+
 </body>
 </html>
